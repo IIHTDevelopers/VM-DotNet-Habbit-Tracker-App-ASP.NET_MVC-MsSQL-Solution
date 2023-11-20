63a6f380a935f481664d90077fc9d8659cdc5c48
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-11-20 19:36:08 +0530

    Add files via upload

---
+++ HabitTrackerApp/DAL/Repository/HabitTrackerService.cs
@@ -0,0 +1,47 @@
+﻿using HabitTrackerApp.DAL.Interface;
+using HabitTrackerApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HabitTrackerApp.DAL.Repository
+{
+    public class HabitTrackerService : IHabitTrackerInterface
+    {
+        private IHabitTrackerRepository _repo;
+        public HabitTrackerService(IHabitTrackerRepository repo)
+        {
+            this._repo = repo;
+        }
+
+        public int DeleteHabit(int HabitId)
+        {
+            var res= _repo.DeleteHabit(HabitId);
+            return res;
+        }
+
+        public Habit GetHabitByID(int HabitId)
+        {
+            return _repo.GetHabitByID(HabitId);
+        }
+        public void Save()
+        {
+            _repo.Save();
+        }
+
+
+        IEnumerable<Habit> IHabitTrackerInterface.GetHabits()
+        {
+            return _repo.GetHabits();
+        }
+
+        Habit IHabitTrackerInterface.InsertHabit(Habit Habit)
+        {
+            return _repo.InsertHabit(Habit);
+        }
+
+        bool IHabitTrackerInterface.UpdateHabit(Habit Habit)
+        {
+            return _repo.UpdateHabit(Habit);
+        }
+    }
+}
