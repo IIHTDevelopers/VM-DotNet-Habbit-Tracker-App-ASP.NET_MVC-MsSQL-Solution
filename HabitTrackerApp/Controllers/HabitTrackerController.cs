0cc41342da216b34e9188ecc85a2d5074468ef6e
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-11 11:13:12 +0530

    Add files via upload

---
+++ HabitTrackerApp/Controllers/HabitTrackerController.cs
@@ -0,0 +1,116 @@
+﻿using HabitTrackerApp.DAL.Interface;
+using HabitTrackerApp.DAL.Repository;
+using HabitTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HabitTrackerApp.Controllers
+{
+    public class HabitTrackerController : Controller
+    {
+        private readonly IHabitTrackerInterface _Repository;
+        public HabitTrackerController(IHabitTrackerInterface service)
+        {
+            _Repository = service;
+        }
+        public HabitTrackerController()
+        {
+            // Constructor logic, if needed
+        }
+        // GET: HabitTracker
+        public ActionResult Index()
+        {
+            var Habits = from work in _Repository.GetHabits()
+                        select work;
+            return View(Habits);
+        }
+
+        public ViewResult Details(int id)
+        {
+            Habit Habit =   _Repository.GetHabitByID(id);
+            return View(Habit);
+        }
+
+        public ActionResult Create()
+        {
+            return View(new Habit());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Habit Habit)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _Repository.InsertHabit(Habit);
+                    _Repository.Save();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(Habit);
+        }
+
+        public ActionResult EditAsync(int id)
+        {
+            Habit Habit =  _Repository.GetHabitByID(id);
+            return View(Habit);
+        }
+        [HttpPost]
+        public ActionResult Edit(Habit Habit)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _Repository.UpdateHabit(Habit);
+                    _Repository.Save();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(Habit);
+        }
+
+        public ActionResult Delete(int id, bool? saveChangesError)
+        {
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Unable to save changes. Try again, and if the problem persists see your system administrator.";
+            }
+            Habit Habit =  _Repository.GetHabitByID(id);
+            return View(Habit);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                Habit Habit =  _Repository.GetHabitByID(id);
+                _Repository.DeleteHabit(id);
+                _Repository.Save();
+            }
+            catch (DataException)
+            {
+                return RedirectToAction("Delete",
+                   new System.Web.Routing.RouteValueDictionary {
+        { "id", id },
+        { "saveChangesError", true } });
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ HabitTrackerApp/DAL/Repository/HabitTrackerRepository.cs
@@ -0,0 +1,64 @@
+﻿using HabitTrackerApp.DAL.Interface;
+using HabitTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace HabitTrackerApp.DAL.Repository
+{
+    public class HabitTrackerRepository : IHabitTrackerRepository
+    {
+        private HabitTrackerDbContext _context;
+        public HabitTrackerRepository(HabitTrackerDbContext Context)
+        {
+            this._context = Context;
+        }
+        public IEnumerable<Habit> GetHabits()
+        {
+             return _context.Habits.ToList();
+        }
+        public Habit GetHabitByID(int id)
+        {
+            return _context.Habits.Find(id);
+        }
+        public Habit InsertHabit(Habit Habit)
+        {
+            return _context.Habits.Add(Habit);
+        }
+        public int DeleteHabit(int HabitID)
+        {
+            Habit Habit = _context.Habits.Find(HabitID);
+            var res= _context.Habits.Remove(Habit);
+            return res.Id;
+        }
+        public bool UpdateHabit(Habit Habit)
+        {
+            var res= _context.Entry(Habit).State = EntityState.Modified;
+            return res.Equals("Habit");
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
